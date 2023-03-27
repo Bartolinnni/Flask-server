771947116e7489d04d642213b337efeeec81edd7
Bartolinnni <bartoszr75@gmail.com>
2023-03-27 16:27:35 +0200

    post-mess

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request,flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        flash('Konto zostało założone', category = 'success')
     return render_template("regist.html")
